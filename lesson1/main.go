de3f99fb82322686776dccf8fc4911ba98476eb7
snarksliveshere <snarksliveshere@gmail.com>
2019-09-05 10:57:37 +0300

    lesson_1_dz

--- lesson1/main.go
+++ lesson1/main.go
@@ -1,1 +1,16 @@
-package lesson1
+package main
+
+import (
+	"fmt"
+	"github.com/beevik/ntp"
+	"os"
+)
+
+func main() {
+	time, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	if err != nil {
+		os.Stderr.WriteString(err.Error() + "\n")
+		os.Exit(2)
+	}
+	fmt.Println(time)
+}
