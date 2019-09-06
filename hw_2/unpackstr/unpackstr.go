c1d8e70a6a181171c7884ef9731c065ab160410e
snarksliveshere <snarksliveshere@gmail.com>
2019-09-06 15:55:21 +0300

    hw_2 basic

--- hw_2/unpackstr/unpackstr.go
+++ hw_2/unpackstr/unpackstr.go
@@ -1,10 +1,47 @@
 package unpackstr
 
 import (
+	"errors"
+	"regexp"
 	"strconv"
+	"strings"
 )
 
-func CheckStrCorrect(str string) bool {
+func GetUnpackString(str string) (res string, err error) {
+	if checkStrCorrect(str) == false {
+		return res, errors.New("bad bad1")
+	}
+	numIndex := getAllNumIndex(str)
+	storage := createStorage(numIndex, str)
+
+	if res := returnUnpackStr(str, storage); res != "" {
+		return res, nil
+	}
+	return res, errors.New("bad bad2")
+}
+
+func returnUnpackStr(str string, storage map[int]string) string {
+	var s string
+	for i, v := range str {
+		if _, err := strconv.Atoi(string(v)); err == nil {
+			continue
+		}
+		if val, ok := storage[i]; ok {
+			s += val
+			continue
+		}
+		s += string(v)
+	}
+
+	return s
+}
+
+func getAllNumIndex(str string) [][]int {
+	re := regexp.MustCompile("[0-9]+")
+	return re.FindAllStringIndex(str, -1)
+}
+
+func checkStrCorrect(str string) bool {
 	if str == "" {
 		return false
 	}
@@ -16,4 +53,25 @@ func CheckStrCorrect(str string) bool {
 	}
 
 	return true
+}
+
+func createStorage(collection [][]int, str string) map[int]string {
+	storage := make(map[int]string)
+	for _, v := range collection {
+		ind := v[0] - 1
+		a := str[v[0]:v[1]]
+		// if a0b2 == bb, then a01b2 == bb
+		if string(a[0]) == "0" {
+			storage[ind] = ""
+			continue
+		} else {
+			num, er := strconv.Atoi(a)
+			if er != nil {
+				continue
+			}
+			storage[ind] = strings.Repeat(string(str[ind]), num)
+		}
+	}
+
+	return storage
 }
--- hw_2/unpackstr/unpackstr_test.go
+++ hw_2/unpackstr/unpackstr_test.go
@@ -6,10 +6,10 @@ import (
 )
 
 func TestCheckStrCorrect(t *testing.T) {
-	require.Equal(t, CheckStrCorrect("3olala"), false, "Must be an error if the first symbol is num")
-	require.Equal(t, CheckStrCorrect("olala"), true, "Must be OK")
-	require.Equal(t, CheckStrCorrect("ASDlala"), true, "Must be OK")
-	require.Equal(t, CheckStrCorrect("45"), false, "Must be an error if string can be convert to num")
-	require.Equal(t, CheckStrCorrect("0"), false, "Must be an error if string equal to zero")
-	require.Equal(t, CheckStrCorrect(""), false, "Must be an error if the string is empty")
+	require.Equal(t, checkStrCorrect("3olala"), false, "Must be an error if the first symbol is num")
+	require.Equal(t, checkStrCorrect("olala"), true, "Must be OK")
+	require.Equal(t, checkStrCorrect("ASDlala"), true, "Must be OK")
+	require.Equal(t, checkStrCorrect("45"), false, "Must be an error if string can be convert to num")
+	require.Equal(t, checkStrCorrect("0"), false, "Must be an error if string equal to zero")
+	require.Equal(t, checkStrCorrect(""), false, "Must be an error if the string is empty")
 }
