5b9ab00024e6159f1a5d4c3a030168a60218b49f
snarksliveshere <snarksliveshere@gmail.com>
2019-09-29 12:03:11 +0300

    hw_5: something doesn`t work

--- hw_5/main.go
+++ hw_5/main.go
@@ -16,7 +16,7 @@ func (t *tickT) testTick(i int) error {
 		case <-ticker.C:
 			rand.Seed(time.Now().Unix())
 			i := rand.Intn(100)
-			fmt.Println(i)
+			fmt.Println("inn")
 			if i%2 != 0 {
 				return errors.New("err cause odd")
 			} else {
@@ -26,53 +26,120 @@ func (t *tickT) testTick(i int) error {
 	}
 }
 
-func main() {
-	fmt.Println("olala")
-	//test1()
-	t := new(tickT)
-	err2 := t.testTick(2)
-	if err2 != nil {
-		fmt.Println("its error testtick")
-	} else {
-		fmt.Println("its ok testtick")
-	}
-	err := test2()
-	if err != nil {
-		fmt.Println("its error")
-	} else {
-		fmt.Println("its ok")
+func gogo(sl []func() error, toGo, numErrs int) {
+	fmt.Println(len(sl), toGo)
+	if toGo >= len(sl) {
+		return
 	}
 
-	ticker := time.NewTicker(2 * time.Second)
-
-	ch := make(chan string, 1)
+	errCh := make(chan error)
+	for i := 0; i <= toGo; i++ {
+		go func() {
+			err := sl[i]()
+			if err != nil {
+				errCh <- err
+			}
+		}()
+	}
 
 	go func() {
-		//time.Sleep(4 * time.Second)
-		ch <- "Hello"
-		close(ch)
-	}()
-	timer := time.NewTimer(3 * time.Second)
-	select {
-	case data := <-ch:
-		fmt.Printf("received %v", data)
-	case <-timer.C:
-
-	}
-	for {
-		select {
-		case <-ticker.C:
-			fmt.Println("something")
-			rand.Seed(time.Now().Unix())
-			i := rand.Intn(100)
-			fmt.Println("failed to receive in  4s", i, i%2)
-			//default:
-			//	fmt.Println("default ticker")
+		for x := range errCh {
+			fmt.Println(x.Error())
 		}
-		//time.Sleep(5 * time.Second)
+	}()
+
+}
+
+func main() {
+	fmt.Println("start")
+	//ff := func() error {
+	//	f1 := new(tickT)
+	//	return f1.testTick(2)
+	//}
+	//var tsl []func() error
+	//tsl = append(tsl, ff)
+	//tsl[0]()
+	//errCh := make(chan error)
+	//test1()
+	//t := new(tickT)
+	//err2 := t.testTick(2)
+	sl := make([]func() error, 0)
+	for i := 1; i < 5; i++ {
+		foo := func() error {
+			f := new(tickT)
+			return f.testTick(i)
+		}
+		sl = append(sl, foo)
+	}
+	err := sl[0]()
+	if err != nil {
+		fmt.Println("ola err")
+	} else {
+		fmt.Println("ola ok")
+
 	}
 
-	test()
+	gogo(sl, 2, 3)
+	//fmt.Println(sl)
+	//for _, v := range sl {
+	//	go func() {
+	//		fmt.Println("olala")
+	//		err := v()
+	//		if err != nil {
+	//			errCh <- err
+	//		}
+	//	}()
+	//}
+	//
+	//go func() {
+	//	for x := range errCh {
+	//		fmt.Println(x.Error())
+	//	}
+	//
+	//}()
+
+	//if err2 != nil {
+	//	fmt.Println("its error testtick")
+	//} else {
+	//	fmt.Println("its ok testtick")
+	//}
+	//err := test2()
+	//if err != nil {
+	//	fmt.Println("its error")
+	//} else {
+	//	fmt.Println("its ok")
+	//}
+	//
+	//ticker := time.NewTicker(2 * time.Second)
+	//
+	//ch := make(chan string, 1)
+	//
+	//go func() {
+	//	//time.Sleep(4 * time.Second)
+	//	ch <- "Hello"
+	//	close(ch)
+	//}()
+	//timer := time.NewTimer(3 * time.Second)
+	//select {
+	//case data := <-ch:
+	//	fmt.Printf("received %v", data)
+	//case <-timer.C:
+	//
+	//}
+	//for {
+	//	select {
+	//	case <-ticker.C:
+	//		fmt.Println("something")
+	//		rand.Seed(time.Now().Unix())
+	//		i := rand.Intn(100)
+	//		fmt.Println("failed to receive in  4s", i, i%2)
+	//		//default:
+	//		//	fmt.Println("default ticker")
+	//	}
+	//	//time.Sleep(5 * time.Second)
+	//}
+	//
+	//test()
 }
 
 func test() (err error) {
