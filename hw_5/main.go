baaa4264606ead000c8a9e2ca972e80e6f6cef29
snarksliveshere <snarksliveshere@gmail.com>
2019-10-04 22:25:15 +0300

    hw_5: next try

--- hw_5/main.go
+++ hw_5/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"runtime"
 	"sync"
 	"time"
 )
@@ -22,99 +22,49 @@ func (t *tickT) testTimer(i int) error {
 	for {
 		select {
 		case <-timer.C:
-			return nil
-			//return errors.New("just err")
-		}
-	}
-}
-
-func gogo(sl []func() error, toGo, numErrs int) {
-	if toGo >= len(sl) {
-		return
-	}
-	errCh := make(chan int, numErrs)
-	for i := 0; i <= toGo; i++ {
-		go func(i int) {
-			for {
-				if sl[i]() != nil {
-					select {
-					case errCh <- i:
-					default:
-						return
-					}
-				}
-
-			}
-		}(i)
-	}
-	for {
-		select {
-		case <-time.After(time.Millisecond * 100):
-			fmt.Println(len(errCh), "len errch", time.Now())
-			fmt.Printf("Runtime numGoroutine %d\n", runtime.NumGoroutine())
-			// 1 горутина - это main, следовательно, все остальные отвалились
-			if runtime.NumGoroutine() == 1 {
-				return
-			}
+			//return nil
+			return errors.New("just err")
 		}
 	}
 }
 
 func go2(sl []func() error, numOfRoutines, numOfErrors int) {
 	tasks := make(chan func() error, numOfFunc)
-	errCh := make(chan int)
-	die := make(chan bool)
+	errCh := make(chan int, numOfErrors)
 	for _, v := range sl {
 		tasks <- v
 	}
-	time.Sleep(1 * time.Second)
 	var wg sync.WaitGroup
 	for i := 0; i < numOfRoutines; i++ {
-		fmt.Println(i, "ola")
 		wg.Add(1)
-		go worker(tasks, errCh, die, &wg)
+		go func(i int) {
+			go worker(tasks, errCh, &wg, numOfErrors, len(sl), i)
+		}(i)
+
 	}
 
-	for {
-		select {
-		case <-time.After(time.Millisecond * 100):
-			fmt.Println("numof routines", runtime.NumGoroutine(), time.Now())
-		}
-	}
 	wg.Wait()
-	fmt.Println("ola")
-
-	//var i int
-	//for {
-	//	select {
-	//
-	//	case <-errCh:
-	//		i++
-	//		fmt.Println(len(errCh))
-	//		if i >= 5 {
-	//			die <- true
-	//		}
-	//case <-time.After(time.Millisecond * 100):
-	//	fmt.Println(len(errCh), "len errch", time.Now())
-	//	fmt.Printf("Runtime numGoroutine %d\n", runtime.NumGoroutine())
-	// 1 горутина - это main, следовательно, все остальные отвалились
-	//if runtime.NumGoroutine() == 1 {
-	//	return
-	//}
-	//}
-	//}
 }
 
-func worker(tasks <-chan func() error, errCh chan int, die <-chan bool, wg *sync.WaitGroup) {
+func worker(tasks <-chan func() error, errCh chan int, wg *sync.WaitGroup, numOfErrs, sliceLength, index int) {
 	defer wg.Done()
-	fmt.Println("working")
+	die := make(chan bool, sliceLength)
+
 	for {
+		// index передаю просто для наглядности
+		fmt.Println("start working routine # ", index)
 		select {
 		case <-die:
+			fmt.Println("buy!")
 			return
 		case f := <-tasks:
 			if f() != nil {
-				fmt.Println(len(errCh), "err length")
+				fmt.Println("scratch up errors", len(errCh))
+				if len(errCh) == numOfErrs {
+					die <- true
+					fmt.Println("good buy!")
+					return
+				}
 				errCh <- 1
 			}
 		default:
@@ -137,6 +87,5 @@ func main() {
 	}
 	go2(sl, numOfRoutines, numOfErrs)
 
-	//gogo(sl, numOfRoutines, numOfErrs)
-	//fmt.Println("end main")
+	fmt.Println("end working")
 }
