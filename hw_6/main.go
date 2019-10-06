f8b83f1642b57313343fdbd0ebb0cb14c0f4defb
snarksliveshere <snarksliveshere@gmail.com>
2019-10-06 13:25:56 +0300

    hw_6: add flag

--- hw_6/main.go
+++ hw_6/main.go
@@ -1,24 +1,39 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
 
-const (
-	offset = 128
-	ifPath = "./if.txt"
-	ofPath = "./of.txt"
-	limit  = 4
+//const (
+//	offset = 128
+//	//ifPath = "./if.txt"
+//	//ofPath = "./of.txt"
+//	//limit  = 4
+//)
+
+var (
+	from, to      string
+	offset, limit int64
 )
 
+func init() {
+	flag.StringVar(&from, "from", "./if.txt", "if file")
+	flag.StringVar(&to, "to", "./of.txt", "of file")
+	flag.Int64Var(&offset, "offset", 0, "offset")
+	flag.Int64Var(&limit, "limit", 0, "limit")
+}
+
 func main() {
-	ifFile, _ := os.Open(ifPath)
+	flag.Parse()
+	fmt.Println(from, to, offset, limit)
+	ifFile, _ := os.Open(from)
 	defer func() { _ = ifFile.Close() }()
 	b := make([]byte, limit)
 	ifFile.ReadAt(b, offset)
 	fmt.Println(string(b))
-	ofFile, _ := os.Create(ofPath)
+	ofFile, _ := os.Create(to)
 	ofFile.Write(b)
 
 	//ifFile.Seek(offset, io.SeekStart)
