a2b3beba19824e1e43e3dc4a1e96c940700d8807
snarksliveshere <snarksliveshere@gmail.com>
2019-11-09 15:19:43 +0300

    hw_9: intrude logger & config

--- hw_8/pkg/logger.go
+++ hw_8/pkg/logger.go
@@ -1,11 +1,31 @@
 package pkg
 
 import (
-	"log"
+	"github.com/sirupsen/logrus"
+	"github.com/snarskliveshere/otus_golang/hw_8/config"
 )
 
-type Logger struct{}
+const appName = "simple_app_calendar"
 
-func (logger Logger) Log(args ...interface{}) {
-	log.Println(args...)
+type Logger struct {
+	log *logrus.Entry
+}
+
+func (logger *Logger) Log() *logrus.Entry {
+	return logger.log
+}
+
+func CreateLog(config *config.Config) *Logger {
+
+	log := logrus.New()
+	logEntry := logrus.NewEntry(log).WithField("app", appName)
+	level, err := logrus.ParseLevel(config.LogLevel)
+	if err != nil {
+		log.Fatal("An error occurred during the logLevelAssertion")
+	}
+	log.SetLevel(level)
+
+	return &Logger{log: logEntry}
+
+	//return logEntry
 }
