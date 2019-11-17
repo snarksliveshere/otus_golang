779a76996c10eaade8223bed31dc6088a2f39a6b
snarksliveshere <snarksliveshere@gmail.com>
2019-11-17 16:09:08 +0300

    hw_11: move routes

---
+++ hw_11_calendar_http/cmd/web/routers.go
@@ -0,0 +1,65 @@
+package web
+
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+func routesRegister(router *mux.Router) {
+	router.HandleFunc("/", helloHandler)
+	router.HandleFunc("/create-event", createEventHandler)
+	router.HandleFunc("/update-event", updateEventHandler)
+	router.HandleFunc("/delete-event", deleteEventHandler)
+	router.HandleFunc("/events-for-day", eventsForDayHandler)
+	router.HandleFunc("/events-for-week", eventsForWeekHandler)
+	router.HandleFunc("/events-for-month", eventsForMonthHandler)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("hello"))
+	if err != nil {
+		log.Log().Fatal("An error occurred")
+	}
+}
+
+func createEventHandler(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("hello"))
+	if err != nil {
+		log.Log().Fatal("An error occurred")
+	}
+}
+
+func updateEventHandler(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("hello"))
+	if err != nil {
+		log.Log().Fatal("An error occurred")
+	}
+}
+
+func deleteEventHandler(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("hello"))
+	if err != nil {
+		log.Log().Fatal("An error occurred")
+	}
+}
+
+func eventsForDayHandler(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("hello"))
+	if err != nil {
+		log.Log().Fatal("An error occurred")
+	}
+}
+
+func eventsForWeekHandler(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("hello"))
+	if err != nil {
+		log.Log().Fatal("An error occurred")
+	}
+}
+
+func eventsForMonthHandler(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("hello"))
+	if err != nil {
+		log.Log().Fatal("An error occurred")
+	}
+}
--- hw_11_calendar_http/cmd/web/server.go
+++ hw_11_calendar_http/cmd/web/server.go
@@ -27,69 +27,12 @@ func Server(path string) {
 func webApi(conf *config.Config) {
 	listenAddr := conf.ListenAddr()
 	router := mux.NewRouter()
-	router.HandleFunc("/", helloHandler)
-	router.HandleFunc("/create-event", createEventHandler)
-	router.HandleFunc("/update-event", updateEventHandler)
-	router.HandleFunc("/delete-event", deleteEventHandler)
-	router.HandleFunc("/events-for-day", eventsForDayHandler)
-	router.HandleFunc("/events-for-week", eventsForWeekHandler)
-	router.HandleFunc("/events-for-month", eventsForMonthHandler)
-
+	routesRegister(router)
 	webServer := &http.Server{Addr: listenAddr, Handler: router}
-
 	go func() {
 		if err := webServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			log.Log().Panic(err)
 		}
 	}()
 	log.Log().Infof("Run web webApi server: %v", listenAddr)
-}
-
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	_, err := w.Write([]byte("hello"))
-	if err != nil {
-		log.Log().Fatal("An error occurred")
-	}
-}
-
-func createEventHandler(w http.ResponseWriter, r *http.Request) {
-	_, err := w.Write([]byte("hello"))
-	if err != nil {
-		log.Log().Fatal("An error occurred")
-	}
-}
-
-func updateEventHandler(w http.ResponseWriter, r *http.Request) {
-	_, err := w.Write([]byte("hello"))
-	if err != nil {
-		log.Log().Fatal("An error occurred")
-	}
-}
-
-func deleteEventHandler(w http.ResponseWriter, r *http.Request) {
-	_, err := w.Write([]byte("hello"))
-	if err != nil {
-		log.Log().Fatal("An error occurred")
-	}
-}
-
-func eventsForDayHandler(w http.ResponseWriter, r *http.Request) {
-	_, err := w.Write([]byte("hello"))
-	if err != nil {
-		log.Log().Fatal("An error occurred")
-	}
-}
-
-func eventsForWeekHandler(w http.ResponseWriter, r *http.Request) {
-	_, err := w.Write([]byte("hello"))
-	if err != nil {
-		log.Log().Fatal("An error occurred")
-	}
-}
-
-func eventsForMonthHandler(w http.ResponseWriter, r *http.Request) {
-	_, err := w.Write([]byte("hello"))
-	if err != nil {
-		log.Log().Fatal("An error occurred")
-	}
 }
