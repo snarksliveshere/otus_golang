9e35032ae49b78be9d5ab4f42f04087142ca003d
snarksliveshere <snarksliveshere@gmail.com>
2019-11-25 21:13:20 +0300

    hw_12: simple mux)

--- hw_12_grpc/client.go
+++ hw_12_grpc/client.go
@@ -7,50 +7,28 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/status"
 	"log"
+	"os"
 	"time"
 )
 
-//func writeRoutine(end chan interface{}, ctx context.Context, conn chat.ChatExampleClient) {
-//	scanner := bufio.NewScanner(os.Stdin)
-//OUTER:
-//	for {
-//		select {
-//		case <-ctx.Done():
-//			break OUTER
-//		default:
-//			if !scanner.Scan() {
-//				break OUTER
-//			}
-//			str := scanner.Text()
-//			if str == "exit" {
-//				break OUTER
-//			}
-//			msg, err := conn.SendMessage(context.Background(), &chat.ChatMessage{Text: str,})
-//			if err != nil {
-//				fmt.Printf("error:%s\n", status.Convert(err).Message())
-//			}
-//
-//			if msg != nil {
-//				created, _ := ptypes.Timestamp(msg.Created)
-//				fmt.Printf("[%s]id:%d msg:%s\n", created.Local(), msg.Id, msg.Text)
-//			}
-//
-//		}
-//
-//	}
-//	log.Printf("Finished writeRoutine")
-//	close(end)
-//}
-
 func main() {
-
 	ctx, _ := context.WithTimeout(context.Background(), 1*time.Second)
-
 	cc, err := grpc.Dial("0.0.0.0:50052", grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("could not connect: %v", err)
 	}
-	defer cc.Close()
+	defer func() { _ = cc.Close() }()
+
+	switch expr := os.Args[1]; expr {
+	case "create-event":
+		sendCreateMessage(ctx, cc)
+	default:
+		fmt.Println("bad route")
+	}
+
+}
+
+func sendCreateMessage(ctx context.Context, cc *grpc.ClientConn) {
 	c := proto.NewCreateEventServiceClient(cc)
 	message := proto.CreateEventRequestMessage{
 		Title:       "some title",
@@ -58,11 +36,9 @@ func main() {
 		Date:        "2019-11-01",
 	}
 	msg, err := c.SendCreateEventMessage(ctx, &message)
-
 	if err != nil {
 		fmt.Printf("error : %s\n", status.Convert(err).Message())
 	}
-
 	if msg != nil {
 		fmt.Printf("error:%v status:%v\n, record: %#v, id %v", msg.Error, msg.Status, msg.Record, msg.Record.Id)
 	}
