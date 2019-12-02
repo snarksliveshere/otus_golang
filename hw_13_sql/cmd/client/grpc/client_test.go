65e572dcea92024900c61a7b66b29f0d0defd832
snarksliveshere <snarksliveshere@gmail.com>
2019-12-02 23:24:03 +0300

    hw_13: add update test

--- hw_13_sql/cmd/client/grpc/client_test.go
+++ hw_13_sql/cmd/client/grpc/client_test.go
@@ -109,59 +109,76 @@ func TestSendDeleteEventMessage(t *testing.T) {
 	}
 }
 
-//
-//func TestSendUpdateEventMessage(t *testing.T) {
-//	cases := []struct {
-//		status, title, description, date string
-//		updTitle, updDescription         string
-//		plus                             uint64
-//	}{
-//		{
-//			status:         "success",
-//			title:          "some new title",
-//			description:    "some new description",
-//			date:           "2019-11-01",
-//			updTitle:       "update_title",
-//			updDescription: "update_description",
-//		},
-//		{
-//			status:         "error",
-//			title:          "new title2",
-//			description:    "some new description2",
-//			date:           "2019-11-01",
-//			plus:           1,
-//			updTitle:       "update_title",
-//			updDescription: "update_description",
-//		},
-//	}
-//
-//	for _, c := range cases {
-//		ctx, _ := context.WithTimeout(context.Background(), 1*time.Second)
-//		cc, err := grpc.Dial("0.0.0.0:"+config.ConfigPort, grpc.WithInsecure())
-//		if err != nil {
-//			t.Errorf("TestCreateEvent(), resp.status: %s", c.status)
-//		}
-//		msg := proto.CreateEventRequestMessage{
-//			Title:       c.title,
-//			Description: c.description,
-//			Date:        c.date,
-//		}
-//
-//		resp := sendCreateEventMessage(ctx, cc, msg)
-//
-//		updMsg := proto.UpdateEventRequestMessage{
-//			EventId:     resp.Record.Id + c.plus,
-//			Title:       c.updTitle,
-//			Description: c.updDescription,
-//			Date:        c.date,
-//		}
-//		respUpdate := sendUpdateEventMessage(ctx, cc, updMsg)
-//
-//		if c.status != respUpdate.Status {
-//			t.Errorf("TestSendUpdateEventMessage() status compare, c.status: %s, resp.status: %v", c.status, resp.Status)
-//		}
-//	}
-//}
+func TestSendUpdateEventMessage(t *testing.T) {
+	cases := []struct {
+		status, title, description, time string
+		updTitle, updDescription         string
+		plus                             uint64
+	}{
+		{
+			status:         "success",
+			title:          "some new title",
+			description:    "some new description",
+			time:           "2019-02-01T20:03+0300",
+			updTitle:       "update_title",
+			updDescription: "update_description",
+		},
+		{
+			status:         "error",
+			title:          "new title2",
+			description:    "some new description2",
+			time:           "2019-03-01T20:03+0300",
+			plus:           1,
+			updTitle:       "update_title",
+			updDescription: "update_description",
+		},
+	}
+
+	for _, c := range cases {
+		ctx, _ := context.WithTimeout(context.Background(), 1*time.Second)
+		cc, err := grpc.Dial("0.0.0.0:"+config.ConfigPort, grpc.WithInsecure())
+		if err != nil {
+			t.Errorf("TestCreateEvent(), resp.status: %s", c.status)
+		}
+		timeS, err := createTimeStampFromTimeString(c.time)
+		if err != nil {
+			t.Errorf("TestCreateEvent(),  err time: %s", c.time)
+		}
+		msg := proto.CreateEventRequestMessage{
+			Title:       c.title,
+			Description: c.description,
+			Time:        timeS,
+		}
+
+		resp := sendCreateEventMessage(ctx, cc, msg)
+
+		updMsg := proto.UpdateEventRequestMessage{
+			EventId:     resp.Id + c.plus,
+			Title:       c.updTitle,
+			Description: c.updDescription,
+		}
+		respUpdate := sendUpdateEventMessage(ctx, cc, updMsg)
+
+		msgId := proto.GetEventByIdRequestMessage{
+			EventId: resp.Id,
+		}
+		rec := sendGetEventsByIdMessage(ctx, cc, msgId)
+
+		if c.status != respUpdate.Status {
+			t.Errorf("TestSendUpdateEventMessage() status compare, c.status: %s, resp.status: %v", c.status, resp.Status)
+		}
+		if c.updTitle != rec.Record.Title && c.status != respUpdate.Status {
+			t.Errorf("TestSendUpdateEventMessage() title compare, c.title: %s, rec title: %v",
+				c.title, rec.Record.Title)
+		}
+		if c.updDescription != rec.Record.Description && c.status != respUpdate.Status {
+			t.Errorf("TestSendUpdateEventMessage() description compare, c.description: %s, rec description: %v",
+				c.description, rec.Record.Description)
+		}
+
+	}
+}
+
 //
 //func TestSendGetEventsForDayMessage(t *testing.T) {
 //	cases := []struct {
