a61cf790334fa709d0af50fad4f623b8b4972edc
snarksliveshere <snarksliveshere@gmail.com>
2019-12-07 15:25:52 +0300

    hw_14: server out

---
+++ hw_14_rabbit/server/internal/usecases/usecases.go
@@ -0,0 +1,18 @@
+package usecases
+
+import (
+	"github.com/snarksliveshere/otus_golang/hw_14_rabbit/server/entity"
+)
+
+type Logger interface {
+	Info(args ...interface{})
+	Infof(pattern string, args ...interface{})
+	Fatal(args ...interface{})
+	Fatalf(pattern string, args ...interface{})
+}
+
+type Actions struct {
+	EventRepository entity.EventRepository
+	DateRepository  entity.DateRepository
+	Logger          Logger
+}
