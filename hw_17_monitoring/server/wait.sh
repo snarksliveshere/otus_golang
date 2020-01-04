c2d2f6330066dc5888d5e0cf09d586e92a7ba5a0
snarksliveshere <snarksliveshere@gmail.com>
2020-01-04 10:06:43 +0300

    hw_17: rm bin/wait.sh

--- hw_17_monitoring/server/wait.sh
+++ hw_17_monitoring/server/wait.sh
@@ -1,10 +1,29 @@
 #!/bin/sh
 # wait-for-postgres.sh
 
-set -e
+RETRIES=5
 
-host="${1}"
-echo "NOW - HOST"
-echo host
-echo "${REG_SERVICE_LOG_LEVEL}   long This work"
-echo "${LOG_LEVEL} short"
+until psql -h ${REG_SERVICE_DB_HOST} -U ${REG_SERVICE_DB_USER} -d ${REG_SERVICE_DB_NAME} -c "select 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
+  echo "Waiting for postgres server, $((RETRIES--)) remaining attempts..."
+  sleep 2
+done
+
+while psql -h ${REG_SERVICE_DB_HOST} -U ${REG_SERVICE_DB_USER} -d ${REG_SERVICE_DB_NAME} -c "select 1";
+do
+  echo "Waiting for postgres server..."
+  sleep 2;
+ done
+
+done psql -h ${REG_SERVICE_DB_HOST} -U ${REG_SERVICE_DB_USER} -d ${REG_SERVICE_DB_NAME} -c "select 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
+  echo "Waiting for postgres server, $((RETRIES--)) remaining attempts..."
+  sleep 2
+done
+#
+#
+#set -e
+#
+#host="${1}"
+#echo "NOW - HOST"
+#echo host
+#echo "${REG_SERVICE_LOG_LEVEL}   long This work"
+#echo "${LOG_LEVEL} short"
